4e68bb57da78e9aeae829adfde6cbf8086e62e22
Joseph Hyphening <joseph.hyphening@gmail.com>
2026-03-19 15:56:29 +0530

    Hide mobile chat popup

--- src/components/sections/ChatbotSection.tsx
+++ src/components/sections/ChatbotSection.tsx
@@ -281,7 +281,7 @@ export const ChatbotSection = ({ isFloating = false }) => {
               initial={{ opacity: 0, scale: 0.9, y: 20 }}
               animate={{ opacity: 1, scale: 1, y: 0 }}
               exit={{ opacity: 0, scale: 0.9, y: 20 }}
-              className="z-50"
+              className="z-50 hidden md:block"
             >
               {ChatContent}
             </motion.div>
